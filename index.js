671cea4236f0620458f098b1a4afabcbeb6e3390
Michael Tanaka <tanakam4214@gmail.com>
2023-03-01 15:00:30 -0800

    loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayName, eventName) {
+    const thankYou = [];
+    for (let x = 0; x < arrayName.length; x++) {
+        thankYou.push(`Thank you, ${arrayName[x]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYou
+}
+
+function countDown() {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+debugger
+}
